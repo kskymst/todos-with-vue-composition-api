a86c1624717f8df5945cbb98c2e595e3ff39df60
Kosuke Yamashita <yamashita.ksk@gmail.com>
2020-02-24 19:37:46 +0900

    add: sample2 を追加

---
+++ pages/sample2.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <h1>other types</h1>
+    <p>{{ count }}</p>
+    <button @click="increment">
+      up
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent, ref } from '@vue/composition-api'
+const useCounter = () => {
+  const count = ref(0)
+  const increment = () => {
+    count.value++
+  }
+  return { count, increment }
+}
+export default createComponent({
+  setup() {
+    const { count, increment } = useCounter()
+    return { count, increment }
+  }
+})
+</script>
+
+<style>
+.wrapper {
+  width: 960px;
+  margin: 40px auto 0;
+}
+</style>
